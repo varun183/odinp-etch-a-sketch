dd4c165e07bc507d89122e06e8639ad34b9b73b1
varun183 <125751714+varun183@users.noreply.github.com>
2023-12-05 14:15:54 +0530

    create grid size

--- main.js
+++ main.js
@@ -1,28 +1,54 @@
 const gridSide = 600;
+let squareSide = 16;
 
-let rows = 20;
-let cols = 20;
+
+const sizeSelectorBtn = document.querySelector('#sizeSelector');
+sizeSelectorBtn.addEventListener('click', function(){
+    let squareSide = getsquareSide();
+    createGridCells(squareSide);
+})
 
 
 const sketchArea = document.querySelector('.board');
-sketchArea.style.width = `${gridSide}px`;
-sketchArea.style.height = `${gridSide}px`; 
+sketchArea.style.width = sketchArea.style.height = `${gridSide}px`; 
 
+function setCellColor(){
+    this.style.backgroundColor = "black";
+}
 
-function createGridCells(){
-    for(let i =0;i<(rows*cols);i++){
+ 
+function createGridCells(squareSide){
+
+    const numOfSquares = (squareSide*squareSide);
+    const widthOrHeight =  `${(gridSide/squareSide)-2.22}px`;
+    for(let i =0;i<(numOfSquares);i++){
 
         const gridCell = document.createElement("div");
 
-        gridCell.style.width = `${(gridSide/cols)-2.22}px`;
-        gridCell.style.height= `${(gridSide/rows)-2.22}px`;
+        gridCell.style.width =  gridCell.style.height= widthOrHeight;
         gridCell.classList.add("cell");
 
         sketchArea.appendChild(gridCell);
+
+        gridCell.addEventListener("mouseover",setCellColor);
 
 
     }
 
 }
 
-createGridCells();
+function getsquareSide(){
+    let input = prompt("Enter the size of the sketch-area");
+
+    let message = document.querySelector("#message");
+
+    if(input==""){
+        message.textContent ="Error!! Provide a number";
+    }else if(input<4 || input>100){
+        message.textContent = "Invalid input!! Provide a number between 4 and 100"
+    }else{
+        message.textContent="Canvas is ready!!!";
+        return input;
+    }
+}
+
